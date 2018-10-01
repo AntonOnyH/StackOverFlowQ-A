433d1be04ba84859d8763ee4fcd932cb2d5e3c0f
Anton Huisamen <antonmcde@icloud.com>
2018-10-01 17:37:52 +0200

    First bit of UI

---
+++ StackOverFlow4iPhone/StackOverFlow4iPhone/Controllers/SCR02ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SCR02ViewController.swift
+//  StackOverFlow4iPhone
+//
+//  Created by Anton Huisamen on 2018/10/01.
+//  Copyright © 2018 antonsolo. All rights reserved.
+//
+
+import UIKit
+
+class SCR02ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
